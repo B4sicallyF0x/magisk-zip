227a75672c66c114e03c9ac47a4ce7a746f4b83c
Proton0 <107559606+Proton0@users.noreply.github.com>
2024-05-13 16:13:31 +0800

    add license and readme and fix build.sh

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 dos2unix module/META-INF/com/google/android/updater-binary
 #7z a -tzip -mx=0 "Rabbit R1 Launcher.zip" module/*
-zip -0 -ll module
+zip -0 -ll "Rabbit R1 Launcher.zip" module/*
 adb push "Rabbit R1 Launcher.zip" /sdcard
 echo Pushed succesfully
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Rabbit Reborn Magisk ZIP
+
+A magisk module to install all the required files for running the Rabbit R1 Launcher
+
+
+## Installation
+
+Download the latest release and install the .zip
